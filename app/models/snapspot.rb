ea56b2920405a3f607ac46f6744cd5b689969fdb
Sammie <samantha.lhe@gmail.com>
2017-10-13 15:02:08 +0800

    Add association and validation for snapspot

--- app/models/snapspot.rb
+++ app/models/snapspot.rb
@@ -1,2 +1,10 @@
 class Snapspot < ApplicationRecord
+  # associations
+  belongs_to :user
+  # has_many :likes, dependent: :destroy
+  # has_many :comments, dependent: :destroy 
+
+  # validation
+  validates :name,:location,:description presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # associations
   has_many :authentications, dependent: :destroy
-  # has_many :snapspots, dependent: :destroy
+  has_many :snapspots, dependent: :destroy
   # has_many :likes, dependent: :destroy
   # has_many :comments, dependent: :destroy
 

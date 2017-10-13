a30f05ee3fc582285377f1a213de414533780974
Sammie <samantha.lhe@gmail.com>
2017-10-13 11:29:21 +0800

    Add facebook authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def new
   end
-  
+
   def create
     @user = User.find_by_email(params[:username])
     if @user && user.authenticate(params[:password])
@@ -17,6 +17,27 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     redirect_to '/login'
+  end
+
+  def create_from_omniauth
+    auth_hash = request.env["omniauth.auth"]
+    authentication = Authentication.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"]) ||  Authentication.create_with_omniauth(auth_hash)
+
+    # if: previously already logged in with OAuth
+    if authentication.user
+      user = authentication.user
+      authentication.update_token(auth_hash)
+      @next = root_url
+      flash[:success] = "Signed in!"
+      # else: user logs in with OAuth for the first time
+    else
+      user = User.create_with_auth_and_hash(authentication, auth_hash)
+      # you are expected to have a path that leads to a page for editing user details
+      @next = edit_user_path(user)
+      flash[:success] = "User created. Please confirm or edit details"
+    end
+
+    redirect_to @next
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
   # login
-  get    '/login',   to: 'sessions#new'
+  get    '/login',   to: 'sessions#new', as:  "login"
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  # Fb authentication
+  get "/auth/:provider/callback" => "sessions#create_from_omniauth"
+
 end

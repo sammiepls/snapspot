0216b8f922757ff946a94e2883fe39929fa5e02b
Sammie <samantha.lhe@gmail.com>
2017-10-13 10:18:00 +0800

    Create authentication model and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171012032512) do
+ActiveRecord::Schema.define(version: 20171013021656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authentications", force: :cascade do |t|
+    t.string "uid"
+    t.string "token"
+    t.string "provider"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_authentications_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -25,4 +35,5 @@ ActiveRecord::Schema.define(version: 20171012032512) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "authentications", "users"
 end

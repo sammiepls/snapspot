d00a38a7f20cca54abfbc982c3344ea90107d0ff
Sammie <samantha.lhe@gmail.com>
2017-10-13 14:56:07 +0800

    Create snapspot model and migration

---
+++ app/models/snapspot.rb
@@ -0,0 +1,2 @@
+class Snapspot < ApplicationRecord
+end
---
+++ db/migrate/20171013064846_create_snapspots.rb
@@ -0,0 +1,12 @@
+class CreateSnapspots < ActiveRecord::Migration[5.1]
+  def change
+    create_table :snapspots do |t|
+      t.string :name
+      t.string :location
+      t.string :description
+      t.belongs_to :user, index:true, foreign_key:true
+
+      t.timestamps
+    end
+  end
+end

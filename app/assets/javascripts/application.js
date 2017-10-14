7764a6797928fe9b88f1ca59ba0dfa1122d8c85c
Sammie <samantha.lhe@gmail.com>
2017-10-14 14:21:40 +0800

    Add map in snapspots#new

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,11 +22,11 @@
 // })
 
 // Show Google Map
-function initMap() {
+function showMap() {
   var latitude = parseFloat($("#latitude").html());
   var longitude = parseFloat($("#longitude").html());
   var marker_location = {lat: latitude, lng: longitude};
-  var map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementById('show-map'), {
     zoom: 18,
     center: marker_location
   });
@@ -35,5 +35,66 @@ function initMap() {
     map: map
   });
 
-  
 }
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map-new'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 18
+  });
+  infoWindow = new google.maps.InfoWindow;
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent(`<div class='text-center'>Latitude: ${pos.lat} <br> Longitude: ${pos.lng}</div>`);
+      marker = new google.maps.Marker({
+        position: pos,
+        map: map
+      });
+      marker.setPosition(pos);
+      infoWindow.open(map, marker);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+
+  map.addListener('click', function(e) {
+    placeMarkerAndPanTo(e.latLng, map);
+  });
+
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+  }
+
+  function placeMarkerAndPanTo(latLng, map) {
+    if ( marker ) {
+      infoWindow.setContent(`<div class='text-center'>Latitude: ${latLng.lat()} <br> Longitude: ${latLng.lng()}</div>`);
+      marker.setPosition(latLng);
+      infoWindow.open(map, marker);
+
+    } else {
+      marker = new google.maps.Marker({
+        position: latLng,
+        map: map
+      });
+
+    }
+    map.panTo(latLng);
+  }

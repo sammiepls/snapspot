3a329364cb34501200e2a3e16979714aa4be5f04
Sammie <samantha.lhe@gmail.com>
2017-10-16 21:41:14 +0800

    Move to production.rb

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,7 @@ module SnapspotApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.assets.initialize_on_precompile = false
-    config.serve_static_assets = true
+  
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers

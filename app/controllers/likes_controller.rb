2138639dc4fbc576767d7ac783f715f0788865cc
Sammie <samantha.lhe@gmail.com>
2017-10-17 12:22:26 +0800

    Setup likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,16 @@
 class LikesController < ApplicationController
+  respond_to :js
+
+  def like
+    @user = current_user
+    @snapspot = Like.find(params[:snapspot_id])
+    @user.like!(@snapspot)
+  end
+
+  def unlike
+    @user = current_user
+    @like = @user.likes.find_by_snapspot_id(params[:snapspot_id])
+    @snapspot = Like.find(params[:snapspot_id])
+    @like.destroy!
+  end
 end
